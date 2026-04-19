2e8d1a017ea018720b288f58add42298145353c4
Alejandro BM <dalebom64@outlook.com>
2026-04-18 19:44:02 -0500

    Subida de proyecto

---
+++ src/main/java/trabajito/WebOnes/Sistema_de_administracion/SistemaDeAdministracionApplication.java
@@ -0,0 +1,13 @@
+package trabajito.WebOnes.Sistema_de_administracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAdministracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAdministracionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trabajito/WebOnes/Sistema_de_administracion/SistemaDeAdministracionApplicationTests.java
@@ -0,0 +1,13 @@
+package trabajito.WebOnes.Sistema_de_administracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeAdministracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
